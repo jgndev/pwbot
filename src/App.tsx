b601a26c3342ff488e552e8af9fc37e0803a6b14
Jeremy Novak <olepunchy@gmail.com>
2023-05-04 08:06:52 -0500

    Updated theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from './components/Header';
-import PasswordGenerator from "./components/Page";
+import PasswordGenerator from "./components/PasswordGenerator";
 
 const App = () => {
   return (
