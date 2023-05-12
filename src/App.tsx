ea3a4628c0c03400649d424f4e774b68e4b073df
Jeremy Novak <olepunchy@gmail.com>
2023-05-12 11:41:28 -0500

    Removed logo in layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from './components/Header';
 import PasswordGenerator from "./components/PasswordGenerator";
 
 const App = () => {
   return (
     <main>
-      <Header />
       <PasswordGenerator />
     </main>
   );
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className="header">
-        <img src="logo.svg" alt="Site Logo" />
-    </div>
-  );
-};
-
-export default Header;
